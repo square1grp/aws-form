c0cd8fbce12e0f3cf62d2f1fd8e6e990817bb918
square1grp <developer01@square1grp.com>
2020-06-17 13:46:55 -0700

    define getOrder

---
+++ modules.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getOrder: (orderId) => {
+    console.log(orderId)
+  }
+}
--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
+const modules = require('./modules')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.post('/', (req, res) => {
+  switch (req.body.action) {
+    case 'check-order':
+      params = JSON.parse(req.body.json)
+
+      modules.getOrder(params.order)
+
+      res.send('')
+      break
+    default:
+      res.send('"action" is not determined.')
+  }
+})
+
+app.listen(port, () => console.log(`AWS MWS Form app listening at http://localhost:${port}`))
