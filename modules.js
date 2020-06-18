c972ef74855fc5b8ee965ae45d9fa3a6eb85091e
square1grp <developer01@square1grp.com>
2020-06-18 03:45:35 -0700

    complete

--- modules.js
+++ modules.js
@@ -1,5 +1,32 @@
+require('dotenv').config()
+const axios = require('axios')
+const moment = require('moment')
+const crypto = require('crypto');
+var amazonMws = require('amazon-mws')(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY)
+
 module.exports = {
   getOrder: (orderId) => {
-    console.log(orderId)
+    return new Promise(resolve => {
+      amazonMws.orders.search({
+        'Version': '2013-09-01',
+        'Action': 'ListOrderItems',
+        'SellerId': process.env.AWS_SELLER_ID,
+        'AmazonOrderId': orderId
+      }, (err, res) => {
+        if (err) {
+          resolve([])
+        } else {
+          try {
+            resolve(res.OrderItems['OrderItem'])
+          } catch (error) {
+            resolve([])
+          }
+        }
+      })
+    });
+  },
+
+  saveEmail: (email) => {
+    return true
   }
 }
--- server.js
+++ server.js
@@ -10,13 +10,38 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
+  let params = {}
   switch (req.body.action) {
     case 'check-order':
       params = JSON.parse(req.body.json)
 
-      modules.getOrder(params.order)
+      modules.getOrder(params.order).then(orderItem => {
+        if (orderItem) {
+          res.send({
+            'status': 'success',
+            'order': true,
+            'asin': orderItem.ASIN,
+            'buyer': '',
+            'order': params.order,
+            'sku': orderItem.SellerSKU,
+            'title': orderItem.Title
+          })
+        } else {
+          res.send({
+            'status': 'success',
+            'answer': 'We are unable to&nbsp;find your order. Make sure it’s<br>the&nbsp;order&nbsp;ID for&nbsp;ELLIEVE ORGANICS.<br>If&nbsp;you have placed an&nbsp;order recently, wait<br>for the&nbsp;goods to&nbsp;be delivered.'
+          })
+        }
+      })
+      break
+    case 'save-email':
+      params = JSON.parse(req.body.json)
 
-      res.send('')
+      modules.saveEmail(params.email)
+
+      res.send({
+        'status': 'success'
+      })
       break
     default:
       res.send('"action" is not determined.')
